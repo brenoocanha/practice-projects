0f3db18f105dfd7277d87293a005be10f550d25e
Breno da Silva Ocanha <brenoocanha@hotmail.com>
2024-04-13 17:10:04 -0300

    feat: basic crud on task manager application

---
+++ nodejs/nest/task-management-application/src/tasks/tasks.service.ts
@@ -0,0 +1,66 @@
+import { Injectable } from '@nestjs/common';
+import { Task, TaskStatus } from './task.model';
+import { v4 as uuid } from 'uuid';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
+
+@Injectable()
+export class TasksService {
+  private tasks: Task[] = [];
+
+  getAllTasks(): Task[] {
+    return this.tasks;
+  }
+
+  getTasksWithFilters(filterDto: GetTasksFilterDto): Task[] {
+    const { status, search } = filterDto;
+    let tasks = this.tasks;
+    if (status) {
+      tasks = tasks.filter((task) => task.status === status);
+    }
+
+    if (search) {
+      tasks = tasks.filter((task) => {
+        if (
+          task.title.toLowerCase().includes(search.toLowerCase()) ||
+          task.description.toLowerCase().includes(search.toLowerCase())
+        ) {
+          return true;
+        }
+        return false;
+      });
+    }
+
+    return tasks;
+  }
+
+  getTaskById(id: string): Task {
+    return this.tasks.find((task) => task.id === id);
+  }
+
+  createTask(createTaskDto: CreateTaskDto): Task {
+    const { title, description } = createTaskDto;
+    const task: Task = {
+      id: uuid(),
+      title,
+      description,
+      status: TaskStatus.OPEN,
+    };
+
+    this.tasks.push(task);
+    return task;
+  }
+
+  updateTaskStatus(id: string, status: TaskStatus) {
+    const target = this.tasks.findIndex((task) => task.id === id);
+    this.tasks[target].status = status;
+    return this.tasks[target];
+  }
+
+  deleteTask(id: string): Task {
+    const deleted = this.tasks.find((task) => task.id === id);
+    const index = this.tasks.indexOf(deleted);
+    this.tasks.splice(index, 1);
+    return deleted;
+  }
+}
