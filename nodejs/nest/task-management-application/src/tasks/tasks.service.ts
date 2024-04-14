5f75d4ccb4c85c87811e1c37d095d865fd5bae0d
Breno da Silva Ocanha <brenoocanha@hotmail.com>
2024-04-14 13:21:18 -0300

    nestjs -> task manager -> feat: error handling and validations

--- nodejs/nest/task-management-application/src/tasks/tasks.service.ts
+++ nodejs/nest/task-management-application/src/tasks/tasks.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import { v4 as uuid } from 'uuid';
 import { CreateTaskDto } from './dto/create-task.dto';
@@ -35,7 +35,11 @@ export class TasksService {
   }
 
   getTaskById(id: string): Task {
-    return this.tasks.find((task) => task.id === id);
+    const found = this.tasks.find((task) => task.id === id);
+    if (!found) {
+      throw new NotFoundException(`Task with ID "${id}" not found`);
+    }
+    return found;
   }
 
   createTask(createTaskDto: CreateTaskDto): Task {
@@ -52,15 +56,13 @@ export class TasksService {
   }
 
   updateTaskStatus(id: string, status: TaskStatus) {
-    const target = this.tasks.findIndex((task) => task.id === id);
-    this.tasks[target].status = status;
-    return this.tasks[target];
+    const task = this.getTaskById(id);
+    task.status = status;
+    return task;
   }
 
-  deleteTask(id: string): Task {
-    const deleted = this.tasks.find((task) => task.id === id);
-    const index = this.tasks.indexOf(deleted);
-    this.tasks.splice(index, 1);
-    return deleted;
+  deleteTask(id: string): void {
+    const found = this.getTaskById(id);
+    this.tasks = this.tasks.filter((task) => task.id !== found.id);
   }
 }
