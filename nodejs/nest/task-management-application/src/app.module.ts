8c60960cc8b9393d1a06a9180ae0d0e5ab110fff
Breno da Silva Ocanha <brenoocanha@hotmail.com>
2024-04-14 16:27:37 -0300

    nodejs -> nestjs -> task manager -> feat: persistence - added database to the application

--- nodejs/nest/task-management-application/src/app.module.ts
+++ nodejs/nest/task-management-application/src/app.module.ts
@@ -1,7 +1,28 @@
 import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [TasksModule],
+  imports: [
+    TasksModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'postgres',
+        host: 'localhost',
+        port: 5432,
+        username: 'postgres',
+        password: config.get<string>('POSTGRES_PASSWORD'),
+        database: 'task-management',
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+    }),
+  ],
 })
 export class AppModule {}
