e679fb009ee91e4c5eee91148648fd7988bdd804
Breno da Silva Ocanha <brenoocanha@hotmail.com>
2024-04-16 23:45:58 -0300

    nodejs -> nestjs -> task-manager -> feat: new auth module

--- nodejs/nest/task-management-application/src/app.module.ts
+++ nodejs/nest/task-management-application/src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -23,6 +24,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
         synchronize: true,
       }),
     }),
+    AuthModule,
   ],
 })
 export class AppModule {}
--- nodejs/nest/task-management-application/src/tasks/tasks.module.ts
+++ nodejs/nest/task-management-application/src/tasks/tasks.module.ts
@@ -4,9 +4,14 @@ import { TasksService } from './tasks.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TasksRepository } from './tasks.repository';
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([TasksRepository]), ConfigModule],
+  imports: [
+    TypeOrmModule.forFeature([TasksRepository]),
+    ConfigModule,
+    AuthModule,
+  ],
   controllers: [TasksController],
   providers: [TasksService],
 })
