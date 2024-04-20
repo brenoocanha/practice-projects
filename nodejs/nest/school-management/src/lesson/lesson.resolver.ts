9283723e0edf15c8f8e43768d87f376e66c5a330
Breno da Silva Ocanha <brenoocanha@hotmail.com>
2024-04-20 17:29:09 -0300

    first commit + feat: graphql configuration + typeorm mongo configuration

---
+++ nodejs/nest/school-management/src/lesson/lesson.resolver.ts
@@ -0,0 +1,15 @@
+import { Query, Resolver } from '@nestjs/graphql';
+import { LessonType } from './lesson.type';
+
+@Resolver(() => LessonType)
+export class LessonResolver {
+  @Query(() => LessonType)
+  lesson() {
+    return {
+      id: 'asdaf',
+      name: 'Physics Class',
+      startDate: new Date().toISOString(),
+      endDate: new Date().toISOString(),
+    };
+  }
+}
