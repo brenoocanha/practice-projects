0783257b34c684ea8733ee17c4251bfe5fe45a18
Breno da Silva Ocanha <brenoocanha@hotmail.com>
2024-04-21 13:01:24 -0300

    nodejs -> nest -> school-manager -> feat: project complete

--- nodejs/nest/school-management/src/lesson/lesson.resolver.ts
+++ nodejs/nest/school-management/src/lesson/lesson.resolver.ts
@@ -1,15 +1,52 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import {
+  Args,
+  Mutation,
+  Parent,
+  Query,
+  ResolveField,
+  Resolver,
+} from '@nestjs/graphql';
 import { LessonType } from './lesson.type';
+import { LessonService } from './lesson.service';
+import { CreateLessonInput } from './lesson.input';
+import { AssignStudentsToLessonInput } from './assign-students-to-lesson.input';
+import { StudentService } from '../student/student.service';
+import { Lesson } from './lesson.entity';
 
 @Resolver(() => LessonType)
 export class LessonResolver {
+  constructor(
+    private lessonService: LessonService,
+    private studentService: StudentService,
+  ) {}
   @Query(() => LessonType)
-  lesson() {
-    return {
-      id: 'asdaf',
-      name: 'Physics Class',
-      startDate: new Date().toISOString(),
-      endDate: new Date().toISOString(),
-    };
+  lesson(@Args('id') id: string) {
+    return this.lessonService.getLesson(id);
+  }
+
+  @Query(() => [LessonType])
+  lessons() {
+    return this.lessonService.getLessons();
+  }
+
+  @Mutation(() => LessonType)
+  createLesson(
+    @Args('createLessonInput') createLessonInput: CreateLessonInput,
+  ) {
+    return this.lessonService.createLesson(createLessonInput);
+  }
+
+  @Mutation(() => LessonType)
+  assignStudentsToLesson(
+    @Args('assignStudentsToLessonInput')
+    assignStudentsToLessonInput: AssignStudentsToLessonInput,
+  ) {
+    const { lessonId, studentIds } = assignStudentsToLessonInput;
+    return this.lessonService.assignStudentsToLesson(lessonId, studentIds);
+  }
+
+  @ResolveField()
+  async students(@Parent() lesson: Lesson) {
+    return this.studentService.getManyStudents(lesson.students);
   }
 }
