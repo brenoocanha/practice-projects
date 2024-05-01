c5476c37ce17bbf131490de40b3e352ba68c2f4a
Breno da Silva Ocanha <brenoocanha@hotmail.com>
2024-05-01 16:12:24 -0300

    react-native -> project-aora -> feat: profile screen

--- react-native/project-aora/app/(auth)/sign-up.jsx
+++ react-native/project-aora/app/(auth)/sign-up.jsx
@@ -7,6 +7,7 @@ import FormField from '../../components/FormField';
 import CustomButton from '../../components/CustomButton';
 import { Link, router } from 'expo-router';
 import { createUser } from '../../lib/appwrite';
+import { useGlobalContext } from '../../context/GlobalProvider';
 
 const SignUp = () => {
   const [form, setForm] = useState({
@@ -14,8 +15,9 @@ const SignUp = () => {
     email: '',
     password: '',
   });
-
   const [isSubmiting, setIsSubmiting] = useState(false);
+
+  const { setUser, setIsLoggedIn } = useGlobalContext();
 
   const submit = async () => {
     if (!form.username || !form.email || !form.password) {
@@ -27,7 +29,7 @@ const SignUp = () => {
     try {
       const result = await createUser(form.email, form.password, form.username);
       setUser(result);
-      setIsLogged(true);
+      setIsLoggedIn(true);
       router.replace('/home');
     } catch (error) {
       Alert.alert('Error', error.message);
