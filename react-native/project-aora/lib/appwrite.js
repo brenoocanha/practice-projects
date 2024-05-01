7604d822c0227717afe31eea156e364ace946f89
Breno da Silva Ocanha <brenoocanha@hotmail.com>
2024-05-01 12:09:03 -0300

    react-native -> project-aora -> feat: appwrite logic (auth logic)

---
+++ react-native/project-aora/lib/appwrite.js
@@ -0,0 +1,84 @@
+import {
+  Client,
+  Account,
+  ID,
+  Avatars,
+  Databases,
+  Query,
+} from 'react-native-appwrite';
+
+export const config = {
+  endpoint: 'https://cloud.appwrite.io/v1',
+  platform: 'com.breno.aora',
+  projectId: '66324f6a00397ac958e8',
+  databaseId: '663250510024376cedae',
+  userCollectionId: '6632506b003648952441',
+  videoCollectionId: '66325087001348df08f4',
+  storageId: '6632517d003049e8c3df',
+};
+
+// Init your react-native SDK
+const client = new Client();
+
+client
+  .setEndpoint(config.endpoint)
+  .setProject(config.projectId)
+  .setPlatform(config.platform);
+
+const account = new Account(client);
+const avatars = new Avatars(client);
+const databases = new Databases(client);
+
+export const createUser = async (email, password, username) => {
+  try {
+    const newAccount = await account.create(
+      ID.unique(),
+      email,
+      password,
+      username
+    );
+    if (!newAccount) throw Error;
+    const avatarUrl = avatars.getInitials(username);
+    await signIn(email, password);
+    const newUser = await databases.createDocument(
+      config.databaseId,
+      config.userCollectionId,
+      ID.unique(),
+      {
+        accountId: newAccount.$id,
+        email,
+        username,
+        avatar: avatarUrl,
+      }
+    );
+    return newUser;
+  } catch (error) {
+    console.log(error);
+    throw new Error(error);
+  }
+};
+
+export const signIn = async (email, password) => {
+  try {
+    const session = await account.createEmailSession(email, password);
+    return session;
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+export const getCurrentUser = async () => {
+  try {
+    const currentAccount = await account.get();
+    if (!currentAccount) throw Error;
+    const currentUser = await databases.listDocuments(
+      config.databaseId,
+      config.userCollectionId,
+      [Query.equal('accountId', currentAccount.$id)]
+    );
+    if (!currentUser) throw Error;
+    return currentUser.documents[0];
+  } catch (error) {
+    console.log(error);
+  }
+};
