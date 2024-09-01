2c309d2078c518311d053731f068275ac8f4bfc2
Breno Ocanha <brenoocanha@hotmail.com>
2024-09-01 18:30:19 -0300

    feat: project structure

---
+++ react-native/uber-clone-ryde/app/(auth)/welcome.tsx
@@ -0,0 +1,11 @@
+import { SafeAreaView, Text } from 'react-native';
+
+const Onboarding = () => {
+  return (
+    <SafeAreaView>
+      <Text>Onboarding</Text>
+    </SafeAreaView>
+  );
+};
+
+export default Onboarding;
