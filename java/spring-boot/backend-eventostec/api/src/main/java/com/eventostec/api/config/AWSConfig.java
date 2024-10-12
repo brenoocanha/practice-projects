132728c36575a3a45e1ba012d4d4d16328f19080
Breno Ocanha <brenoocanha@hotmail.com>
2024-10-12 13:05:23 -0300

    feat: unit tests config

--- java/spring-boot/backend-eventostec/api/src/main/java/com/eventostec/api/config/AWSConfig.java
+++ java/spring-boot/backend-eventostec/api/src/main/java/com/eventostec/api/config/AWSConfig.java
@@ -1,7 +1,6 @@
 package com.eventostec.api.config;
 
-import com.amazonaws.auth.AWSStaticCredentialsProvider;
-import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
@@ -11,22 +10,15 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AWSConfig {
 
-    @Value("${aws.accessKeyId}")
-    private String accessKey;
-
-    @Value("${aws.secretKey}")
-    private String secretKey;
-
     @Value("${aws.region}")
     private String awsRegion;
 
     @Bean
     public AmazonS3 createS3Instance() {
-        BasicAWSCredentials awsCreds = new BasicAWSCredentials(accessKey, secretKey);
         return AmazonS3ClientBuilder
                 .standard()
+                .withCredentials(new DefaultAWSCredentialsProviderChain())
                 .withRegion(awsRegion)
-                .withCredentials(new AWSStaticCredentialsProvider(awsCreds))
                 .build();
     }
 }
