b56de4225da393aa0ba601bb9aa67e23d84a5da4
Breno <brenoocanha@hotmail.com>
2025-05-23 23:44:12 -0300

    feat: mongodb model

---
+++ java/spring-boot/livechatms/src/main/java/com/obrenodev/livechatms/model/Message.java
@@ -0,0 +1,23 @@
+package com.obrenodev.livechatms.model;
+
+import java.util.Date;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collection = "messages")
+public class Message {
+    @Id
+    private String id;
+    private String sender;
+    private String receiver;
+    private String text;
+    private Date date;
+}
