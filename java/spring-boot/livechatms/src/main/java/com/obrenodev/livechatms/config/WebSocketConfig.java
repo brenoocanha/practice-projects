800668c98c21f3d6e19c512ae83ff5bca631e29c
Breno <brenoocanha@hotmail.com>
2025-05-23 23:52:41 -0300

    feat: web socket config

---
+++ java/spring-boot/livechatms/src/main/java/com/obrenodev/livechatms/config/WebSocketConfig.java
@@ -0,0 +1,20 @@
+package com.obrenodev.livechatms.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/topics");
+        registry.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/obrenodev-livechat-websocket");
+    }
+}
