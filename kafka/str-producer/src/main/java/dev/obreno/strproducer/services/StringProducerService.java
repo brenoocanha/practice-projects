9ff87c2da9f731f11f9fef1e8db436e9c13c55da
Breno <brenoocanha@hotmail.com>
2025-06-07 14:30:07 -0300

    feat: added logging on send message service

--- kafka/str-producer/src/main/java/dev/obreno/strproducer/services/StringProducerService.java
+++ kafka/str-producer/src/main/java/dev/obreno/strproducer/services/StringProducerService.java
@@ -1,15 +1,32 @@
 package dev.obreno.strproducer.services;
 
-import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Service;
 
-@RequiredArgsConstructor
+import java.util.concurrent.CompletableFuture;
+
+@Log4j2
 @Service
 public class StringProducerService {
     private final KafkaTemplate<String, String> kafkaTemplate;
 
+    public StringProducerService(@Qualifier("kafkaTemplate") KafkaTemplate<String, String> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
     public void sendMessage(String message) {
-        kafkaTemplate.send("str-topic", message);
+        CompletableFuture<SendResult<String, String>> future = kafkaTemplate.send("str-topic", message);
+        future.whenComplete((r, e) -> {
+            if (e != null) {
+                log.error("Error sending message: {}", e.getMessage());
+                return;
+            }
+
+            log.info("Message sent successfully: {}", r.getProducerRecord().value());
+            log.info("Partition {}, Offset {}", r.getRecordMetadata().partition(), r.getRecordMetadata().offset());
+        });
     }
 }
