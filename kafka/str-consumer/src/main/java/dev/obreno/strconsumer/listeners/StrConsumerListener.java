b544dba2defaabfdf3db0f65169861343e21c28d
Breno <brenoocanha@hotmail.com>
2025-06-08 15:10:35 -0300

    feat: added custom listener interface

--- kafka/str-consumer/src/main/java/dev/obreno/strconsumer/listeners/StrConsumerListener.java
+++ kafka/str-consumer/src/main/java/dev/obreno/strconsumer/listeners/StrConsumerListener.java
@@ -1,29 +1,25 @@
 package dev.obreno.strconsumer.listeners;
 
+import dev.obreno.strconsumer.custom.StrConsumerCustomListener;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.kafka.annotation.TopicPartition;
 import org.springframework.stereotype.Component;
 
 @Log4j2
 @Component
 public class StrConsumerListener {
 
-    @KafkaListener(groupId = "group-1",
-            topicPartitions = {
-            @TopicPartition(topic = "str-topic", partitions = {"0"})
-            }
-            , containerFactory = "strContainerFactory")
+    @StrConsumerCustomListener(groupId = "group-1")
     public void create(String message) {
        log.info("CREATE ::: Received Message: {}", message);
     }
 
-    @KafkaListener(groupId = "group-1",
-            topicPartitions = {
-                    @TopicPartition(topic = "str-topic", partitions = {"1"})
-            }
-            , containerFactory = "strContainerFactory")
+    @StrConsumerCustomListener(groupId = "group-1")
     public void log(String message) {
         log.info("LOG ::: Received Message: {}", message);
+    }
+
+    @StrConsumerCustomListener(groupId = "group-2")
+    public void history(String message) {
+        log.info("HISTORY ::: Received Message: {}", message);
     }
 }
