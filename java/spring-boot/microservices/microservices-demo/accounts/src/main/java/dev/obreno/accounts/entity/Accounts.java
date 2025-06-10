a919ad163f3e08f36a77e454d4b5c41ffddeb221
Breno <brenoocanha@hotmail.com>
2025-06-09 23:44:44 -0300

    feat: created entities + repository -> created base entity + created accounts entity + created customer entity + created accounts repository + created customer repository

---
+++ java/spring-boot/microservices/microservices-demo/accounts/src/main/java/dev/obreno/accounts/entity/Accounts.java
@@ -0,0 +1,29 @@
+package dev.obreno.accounts.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class Accounts extends BaseEntity {
+
+    @Column(name = "customer_id")
+    private Long customerId;
+
+    @Id
+    @Column(name = "account_number")
+    private Long accountNumber;
+
+    @Column(name = "account_type")
+    private String accountType;
+
+    @Column(name = "mobile_number")
+    private String mobileNumber;
+
+    @Column(name = "branch_address")
+    private String branchAddress;
+}
