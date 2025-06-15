405f05290edb5a292e9fca571774df6adc653260
Breno <brenoocanha@hotmail.com>
2025-06-15 17:09:11 -0300

    feat: input data validations

--- java/spring-boot/microservices/microservices-demo/accounts/src/main/java/dev/obreno/accounts/dto/AccountsDto.java
+++ java/spring-boot/microservices/microservices-demo/accounts/src/main/java/dev/obreno/accounts/dto/AccountsDto.java
@@ -1,12 +1,20 @@
 package dev.obreno.accounts.dto;
 
 import jakarta.persistence.Column;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
 public class AccountsDto {
 
+    @NotEmpty(message = "AccountNumber cannot be null or empty")
+    @Pattern(regexp = "$|[0-9]{11}", message = "AccountNumber must be 11 digits")
     private Long accountNumber;
+
+    @NotEmpty(message = "AccountType cannot be null or empty")
     private String accountType;
+
+    @NotEmpty(message = "BranchAddress cannot be null or empty")
     private String branchAddress;
 }
