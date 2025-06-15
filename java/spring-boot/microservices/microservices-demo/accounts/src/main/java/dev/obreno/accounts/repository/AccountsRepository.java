f837758532a0a37516ac3a88bc3016723f512b83
Breno <brenoocanha@hotmail.com>
2025-06-15 11:25:21 -0300

    feat: delete API

--- java/spring-boot/microservices/microservices-demo/accounts/src/main/java/dev/obreno/accounts/repository/AccountsRepository.java
+++ java/spring-boot/microservices/microservices-demo/accounts/src/main/java/dev/obreno/accounts/repository/AccountsRepository.java
@@ -2,11 +2,17 @@ package dev.obreno.accounts.repository;
 
 import dev.obreno.accounts.entity.Accounts;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 @Repository
 public interface AccountsRepository extends JpaRepository<Accounts, Long> {
     Optional<Accounts> findByCustomerId(Long customerId);
+
+    @Transactional
+    @Modifying
+    void deleteByCustomerId(Long customerId);
 }
