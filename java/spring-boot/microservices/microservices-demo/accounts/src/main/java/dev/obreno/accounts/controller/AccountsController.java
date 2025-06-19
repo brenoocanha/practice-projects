ef16b90cbf0063ae531e83174af4de5a9a8800b4
Breno <brenoocanha@hotmail.com>
2025-06-18 22:54:58 -0300

    feat: enhanced documentation using @Schema and example data

--- java/spring-boot/microservices/microservices-demo/accounts/src/main/java/dev/obreno/accounts/controller/AccountsController.java
+++ java/spring-boot/microservices/microservices-demo/accounts/src/main/java/dev/obreno/accounts/controller/AccountsController.java
@@ -2,9 +2,12 @@ package dev.obreno.accounts.controller;
 
 import dev.obreno.accounts.constants.AccountsConstants;
 import dev.obreno.accounts.dto.CustomerDto;
+import dev.obreno.accounts.dto.ErrorResponseDto;
 import dev.obreno.accounts.dto.ResponseDto;
 import dev.obreno.accounts.service.IAccountsService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -73,8 +76,17 @@ public class AccountsController {
                     description = "HTTP Status OK"
             ),
             @ApiResponse(
+                    responseCode = "417",
+                    description = "Expectation Failed"
+            ),
+            @ApiResponse(
                     responseCode = "500",
-                    description = "HTTP Status Internal Server Error"
+                    description = "HTTP Status Internal Server Error",
+                    content = @Content(
+                            schema = @Schema(
+                                    implementation = ErrorResponseDto.class
+                            )
+                    )
             )
     })
     @PutMapping("/update")
@@ -86,8 +98,8 @@ public class AccountsController {
                     .body(new ResponseDto(AccountsConstants.STATUS_200, AccountsConstants.MESSAGE_200));
         } else {
             return ResponseEntity
-                    .status(HttpStatus.INTERNAL_SERVER_ERROR)
-                    .body(new ResponseDto(AccountsConstants.STATUS_500, AccountsConstants.MESSAGE_500));
+                    .status(HttpStatus.EXPECTATION_FAILED)
+                    .body(new ResponseDto(AccountsConstants.STATUS_417, AccountsConstants.MESSAGE_417_UPDATE));
         }
     }
 
@@ -101,8 +113,17 @@ public class AccountsController {
                     description = "HTTP Status OK"
             ),
             @ApiResponse(
+                    responseCode = "417",
+                    description = "Expectation Failed"
+            ),
+            @ApiResponse(
                     responseCode = "500",
-                    description = "HTTP Status Internal Server Error"
+                    description = "HTTP Status Internal Server Error",
+                    content = @Content(
+                            schema = @Schema(
+                                    implementation = ErrorResponseDto.class
+                            )
+                    )
             )
     })
     @DeleteMapping("/delete")
@@ -116,8 +137,8 @@ public class AccountsController {
                     .body(new ResponseDto(AccountsConstants.STATUS_200, AccountsConstants.MESSAGE_200));
         } else {
             return ResponseEntity
-                    .status(HttpStatus.INTERNAL_SERVER_ERROR)
-                    .body(new ResponseDto(AccountsConstants.STATUS_500, AccountsConstants.MESSAGE_500));
+                    .status(HttpStatus.EXPECTATION_FAILED)
+                    .body(new ResponseDto(AccountsConstants.STATUS_417, AccountsConstants.MESSAGE_417_DELETE));
         }
     }
 
--- java/spring-boot/microservices/microservices-demo/accounts/src/main/java/dev/obreno/accounts/dto/AccountsDto.java
+++ java/spring-boot/microservices/microservices-demo/accounts/src/main/java/dev/obreno/accounts/dto/AccountsDto.java
@@ -1,20 +1,37 @@
 package dev.obreno.accounts.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.Column;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
+@Schema(
+        name = "Accounts",
+        description = "Schema to hold Account information"
+)
 public class AccountsDto {
 
+    @Schema(
+            description = "Account number of Eazy Bank account",
+            example = "3454433243"
+    )
     @NotEmpty(message = "AccountNumber cannot be null or empty")
     @Pattern(regexp = "$|[0-9]{11}", message = "AccountNumber must be 11 digits")
     private Long accountNumber;
 
+    @Schema(
+            description = "Account type of Eazy Bank account",
+            example = "Savings"
+    )
     @NotEmpty(message = "AccountType cannot be null or empty")
     private String accountType;
 
+    @Schema(
+            description = "Branch address of Eazy Bank account",
+            example = "123 New York"
+    )
     @NotEmpty(message = "BranchAddress cannot be null or empty")
     private String branchAddress;
 }
